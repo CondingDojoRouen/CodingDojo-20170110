72d6cf97c6b0a65bbdac6f12a92dd571569b45c1
Clement Mauger <Clement Mauger>
2017-01-10 17:51:04 +0100

    init

---
+++ KataPotter/KataPotterTests/KataPotterTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingDojo
+{
+    [TestClass]
+    public class KataPotterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
